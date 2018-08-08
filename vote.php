5fb95688ed3ca3dedb0aac78b1a3bd683f1fb183
章凡凡 <zhangfanfan@zhangfanfandeMacBook-Pro.local>
2018-08-08 20:06:29 +0800

    test

--- vote.php
+++ vote.php
@@ -3,7 +3,7 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
 echo("aaaaa")
-$conn = new mysqli("localhost", "root", "123", "eosdb");
+$conn = new mysqli("localhost","root","123","eosdb");
 echo("bbbb")
 
 ?>
