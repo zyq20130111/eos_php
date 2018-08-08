4c6e3491a6d7abe6437e33373c89670a290006a8
章凡凡 <zhangfanfan@zhangfanfandeMacBook-Pro.local>
2018-08-08 20:04:11 +0800

    test

--- vote.php
+++ vote.php
@@ -4,10 +4,7 @@ header("Content-Type: application/json; charset=UTF-8");
 
 echo("aaaaa")
 $conn = new mysqli("localhost", "root", "123", "eosdb");
-echo("aaaaa")
-$conn->close();
-
-echo($outp);
+echo("bbbb")
 
 ?>
 
