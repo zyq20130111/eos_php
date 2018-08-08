9c56310f94bf7626b622eea091bc4d90951c9538
章凡凡 <zhangfanfan@zhangfanfandeMacBook-Pro.local>
2018-08-08 20:24:19 +0800

    test

--- vote.php
+++ vote.php
@@ -6,7 +6,22 @@ $conn = new mysqli("localhost","root","123","eosdb");
 if ($conn->connect_error) {
     die("连接失败: " . $conn->connect_error);
 } 
-echo "连接成功";
+
+$result = $conn->query("SELECT voter,proudcer,date,vote FROM voter_tbl");
+$outp = "";
+while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
+    if ($outp != "") {$outp .= ",";}
+    $outp .= '{"voter":"'  . $rs["voter"] . '",';
+    $outp .= '"proudcer":"'   . $rs["proudcer"] . '",';
+    $outp .= '"date":"'   . $rs["date"] . '",';
+    $outp .= '"vote":"'. $rs["vote"] . '"}'; 
+}
+$outp ='{"records":['.$outp.']}';
+$conn->close();
+
+echo($outp);
+
+
 
 ?>
 
