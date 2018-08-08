6677d9ee2a5cf740ce02f5568c910c0ad2d5d309
章凡凡 <zhangfanfan@zhangfanfandeMacBook-Pro.local>
2018-08-08 19:48:30 +0800

    test

--- test1.php
+++ test1.php
@@ -1,11 +1,6 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-
-$servername = "localhost";
-$username = "root";
-$password = "123";
-$db = "eosdb"
  
 echo "连接"
 ?>
