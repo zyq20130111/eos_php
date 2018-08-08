465f0543323dae6fcdafed6666b31437ad1c06ff
章凡凡 <zhangfanfan@zhangfanfandeMacBook-Pro.local>
2018-08-08 19:33:36 +0800

    test

--- test1.php
+++ test1.php
@@ -10,10 +10,4 @@ $db = "eosdb"
 $conn = new mysqli($servername, $username, $password);
  
 echo "连接"
-// 检测连接
-if ($conn->connect_error) {
-    die("连接失败: " . $conn->connect_error);
-} 
-echo "连接成功";
-
 ?>
