a79453d05461be849bfee9a17f7733e0ce633b18
章凡凡 <zhangfanfan@zhangfanfandeMacBook-Pro.local>
2018-08-08 19:10:41 +0800

    test

--- test1.php
+++ test1.php
@@ -3,4 +3,6 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
 echo("aaaaa")
+$conn = new mysqli("eosdb", "root", "123", "Northwind");
+echo("bbbb")
 ?>
