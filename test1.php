395e1ba9badedba2077b08ad0c0599d7d45f5070
章凡凡 <zhangfanfan@zhangfanfandeMacBook-Pro.local>
2018-08-08 19:17:33 +0800

    test

--- test1.php
+++ test1.php
@@ -2,6 +2,9 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
+$servername = "localhost";
+$username = "root";
+$password = "123";
 // 创建连接
 $conn = new mysqli($servername, $username, $password);
  
