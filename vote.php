fcdd04dd6d98be6b1e1713c71d5cc312a4c94530
章凡凡 <zhangfanfan@zhangfanfandeMacBook-Pro.local>
2018-08-08 18:35:30 +0800

    sss

---
+++ vote.php
@@ -0,0 +1,20 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+$conn = new mysqli("eosdb", "root", "123", "Northwind");
+
+$result = $conn->query("SELECT voter,proudcer, date,vote  FROM voter_tbl");
+
+$outp = "";
+while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
+    if ($outp != "") {$outp .= ",";}
+    $outp .= '{"voter":"'  . $rs["voter"] . '",';
+    $outp .= '"proudcer":"'   . $rs["proudcer"]        . '",';
+    $outp .= '"date":"'. $rs["date"]     . '"}'; 
+}
+$outp ='{"records":['.$outp.']}';
+$conn->close();
+
+echo($outp);
+?>
