ee899bdbfc1cb8484588cb4a828d0c94ee34c214
章凡凡 <zhangfanfan@zhangfanfandeMacBook-Pro.local>
2018-08-08 20:03:02 +0800

    test

--- vote.php
+++ vote.php
@@ -5,12 +5,9 @@ header("Content-Type: application/json; charset=UTF-8");
 echo("aaaaa")
 $conn = new mysqli("localhost", "root", "123", "eosdb");
 echo("aaaaa")
-$result = $conn->query("SELECT voter,proudcer, date,vote  FROM voter_tbl");
-echo("bbbbb")
 $conn->close();
 
 echo($outp);
-*/
 
 ?>
 
