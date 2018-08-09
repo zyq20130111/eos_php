c4f2efbab8ac189ba6b7bbad66d67dd59d2d0d4c
章凡凡 <zhangfanfan@zhangfanfandeMacBook-Pro.local>
2018-08-09 17:23:51 +0800

    account.php

--- account.php
+++ account.php
@@ -25,11 +25,9 @@ function request_post($url = '', $param = '') {
      return $data;
 }
 
-function getAccount($account = ''){
-    $post_data["account_name"] = $account;
-    $res = request_post("http:127.0.0.1:8888/v1/chain/get_account",$post_data);
-    echo($res);   
-}
+$post_data["account_name"] = $account;
+$res = request_post("http:127.0.0.1:8888/v1/chain/get_account",$post_data);
+echo($res);   
 
 echo("ssssss");
 getAccount($account);
