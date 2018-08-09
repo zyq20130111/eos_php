64ec51826ef331402f7cc179012114d212585883
章凡凡 <zhangfanfan@zhangfanfandeMacBook-Pro.local>
2018-08-09 17:35:46 +0800

    account.php

--- account.php
+++ account.php
@@ -17,7 +17,7 @@ function request_post($url = '', $param = '') {
      curl_setopt($ch, CURLOPT_URL,$postUrl);//抓取指定网页
 
      curl_setopt($ch, CURLOPT_HEADER,array(
-		"Content-Type: application/json;charset=utf-8");//设置header
+		"Content-Type: application/json");//设置header
 
      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);//要求结果为字符串且输出到屏幕上
      curl_setopt($ch, CURLOPT_POST, 1);//post提交方式
