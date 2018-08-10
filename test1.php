226adc82648b66f73a13e80d1f1db8c3ab84c5fe
章凡凡 <zhangfanfan@zhangfanfandeMacBook-Pro.local>
2018-08-10 11:58:01 +0800

    vote

--- test1.php
+++ test1.php
@@ -9,14 +9,15 @@ echo "连接";
 $inj_str = "'|and|exec|insert|select|delete|update|count|*|%|chr|mid|master|truncate|char|declare|;|or|-|+|,|drop";
 echo($inj_str);
 $inj_stra = split("|",$inj_str);
-
+$c = count($inj_str);
+/*
 for($i=0 ; $i < count($inj_str) ; $i++)
 {
     if($inj_stra[$i] == strtolower($producer)){
         echo '{"code":500}'
         return;
     }
-}
+}*/
 
 echo "sssss"
 
