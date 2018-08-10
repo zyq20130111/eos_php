54a51f8c7e265ba840253c37631337be3afa4277
章凡凡 <zhangfanfan@zhangfanfandeMacBook-Pro.local>
2018-08-10 11:43:37 +0800

    vote

--- test1.php
+++ test1.php
@@ -1,6 +1,11 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
+
 $conn = new mysqli("localhost","root","123","eosdb"); 
-echo "连接"
+
+echo "连接";
+return;
+echo "sssss"
+
 ?>
