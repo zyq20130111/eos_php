ee87b77c1ad8fba5d2159043c26e679afb2b0d0d
章凡凡 <zhangfanfan@zhangfanfandeMacBook-Pro.local>
2018-08-10 11:51:27 +0800

    vote

--- test1.php
+++ test1.php
@@ -5,7 +5,19 @@ header("Content-Type: application/json; charset=UTF-8");
 $conn = new mysqli("localhost","root","123","eosdb"); 
 
 echo "连接";
-return;
+
+$inj_str = "'|and|exec|insert|select|delete|update|count|*|%|chr|mid|master|truncate|char|declare|;|or|-|+|,|drop";
+echo($inj_str);
+$inj_stra = split("|",$inj_str);
+
+for($i=0 ; $i < count($inj_str) ; $i++)
+{
+    if($inj_stra[$i] == strtolower($producer)){
+        echo '{"code":500}'
+        return;
+    }
+}
+
 echo "sssss"
 
 ?>
