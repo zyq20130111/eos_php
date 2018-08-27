206d22ccbab4bf306ef67507eaadf30eb7e2df83
System Administrator <root@zhangfanfandeMacBook-Pro.local>
2018-08-27 20:50:27 +0800

    sssss

--- create.sh
+++ create.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 # author:zhang gang
 # $0钱包密码 $1创建账号 $2账号 $3公钥1 $4公钥2 $5内存  $6 CPU $7 NET 
+
 ram="\"$6\""
 cpu="\"$7\""
 net="\"$8\""
 cleos wallet unlock --password $1
-createcmd="cleos system newaccount $2 $3 $4 $5 --buy-ram $ram --stake-cpu $cpu --stake-net $net --transfer"
-echo $createcmd
-${createcmd}
+
+cmd="cleos system newaccount $2 $3 $4 $5 --buy-ram $ram --stake-cpu $cpu --stake-net $net --transfer"
+echo ${cmd}|awk '{run=$0;system(run)}'
+
 cleos wallet lock 
