c8fb87a23491ad69b5e400271c95f30b68f7c59c
System Administrator <root@zhangfanfandeMacBook-Pro.local>
2018-08-27 20:34:13 +0800

    sssss

--- create.sh
+++ create.sh
@@ -13,7 +13,7 @@ echo $cpu
 echo $net
 cleos wallet lock
 cleos wallet unlock --password $1
-createcmd ="cleos system newaccount $2 $3 $4 $5 --buy-ram $ram --stake-cpu $cpu --stake-net $net --transfer"
+createcmd="cleos system newaccount $2 $3 $4 $5 --buy-ram $ram --stake-cpu $cpu --stake-net $net --transfer"
 echo createcmd
 {createcmd}
 cleos wallet lock 
