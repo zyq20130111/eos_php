540bd211281144fda2f5323813f383d8623be40d
章凡凡 <zhangfanfan@zhangfanfandeMacBook-Pro.local>
2018-08-27 17:23:56 +0800

    create

---
+++ create.php
@@ -0,0 +1,53 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+function request_get($url = ''){
+
+   $ch = curl_init();
+   $timeout = 5;
+   curl_setopt ($ch, CURLOPT_URL, $url);
+   curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
+   curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+   $file_contents = curl_exec($ch);
+   curl_close($ch);
+
+   return $file_contents;
+}
+
+$wallet-pwd = "PW5Ji9mDxcY83RgQGnooJPQwqMVoSeSABXvff48kevkHYqGV1Ca6T";
+
+$creator = $_GET["creator"];
+$name = $_GET["name"];
+$ownerkey = $_GET["ownerkey"];
+$activekey = $_GET["activekey"];
+$ram = $_GET["ram"];
+$cpu = $_GET["cpu"];
+$net = $GET["net"];
+
+$code = -1;
+
+try{
+
+   $cmd = sprintf("./create.sh %s %s %s %s %s %s %s %s",$wallet-pwd,$creator,$name,$ownerkey,$activekey,$ram,$cpu,$net);
+   exec("./create.sh",$output,$status);
+
+   $url = sprintf("http://127.0.0.1:8002/eos_php/account.php?account=%s,$name);
+   $result = request_get(url);
+
+   if($result != null){
+
+      $res=json_decode($result,true);
+      if($res["account_name"] != null){
+          if(trim($res["account_name"]) == trim($name)){
+               $code = 0
+          }
+      }
+   }
+   echo sprintf('"code":%d',$code);
+}
+catch(Exception $e){
+   echo sprintf('"code":%d',$code);
+}
+
+
