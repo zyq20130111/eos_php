6277f76771207f8bc4786aab3c1d29c24d0bf6d5
System Administrator <root@zhangfanfandeMacBook-Pro.local>
2018-08-29 16:30:29 +0800

    vote

--- create.php
+++ create.php
@@ -37,7 +37,6 @@ try{
    echo $cmd;
    
    shell_exec($cmd);   
-  /*
 
    $url = sprintf("http://127.0.0.1:8002/eos_php/account.php?account=%s,$name);
    $result = request_get(url);
@@ -52,11 +51,10 @@ try{
           }
       }
    }
-   echo sprintf('"code":%d',$code);
-  */
+   echo sprintf('{"code":%d}',$code);
 }
 catch(Exception $e){
-   echo sprintf('"code":%d',$code);
+   echo sprintf('{"code":%d}',$code);
 }
 
 ?>
