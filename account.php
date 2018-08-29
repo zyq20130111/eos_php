7d42a3a0ea42f71736f8188f94e27d19dd217510
System Administrator <root@zhangfanfandeMacBook-Pro.local>
2018-08-29 18:58:38 +0800

    vote

--- account.php
+++ account.php
@@ -33,6 +33,7 @@ function request_post($url = '', $param = '') {
 $post_data = '{"account_name":"' . $account . '"}';
 $output = request_post("http://127.0.0.1:8888/v1/chain/get_account",$post_data);
 $res=json_decode($output,true);
+echo($res);
 echo($output);
 ?>
 
