58e75a0b29b0a22a2c398ac7b37cc4dc8f46c27d
System Administrator <root@zhangfanfandeMacBook-Pro.local>
2018-08-29 17:16:22 +0800

    vote

--- create.php
+++ create.php
@@ -8,7 +8,6 @@ function request_get($url = ''){
    $ch = curl_init();
    $timeout = 5;
    curl_setopt ($ch, CURLOPT_URL, $url);
-   curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
    curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
    $file_contents = curl_exec($ch);
    curl_close($ch);
