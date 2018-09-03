f76fa6e5b5c6d00af27c3e704446b08ebe40795a
System Administrator <root@zhangfanfandeMacBook-Pro.local>
2018-09-03 14:18:56 +0800

    pwd

--- test1.php
+++ test1.php
@@ -4,7 +4,7 @@ header("Content-Type: application/json; charset=UTF-8");
 
 require 'config.php';
 
-$conn = new mysqli("localhost",Config::USERNAME,Config::PWD,Config::DB);
+$conn = new mysqli("127.0.0.1",Config::USERNAME,Config::PWD,Config::DB);
 
 echo "连接";
 
