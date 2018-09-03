a84ebfd56349b8675d436394e224dbb537171ee4
System Administrator <root@zhangfanfandeMacBook-Pro.local>
2018-09-03 13:53:35 +0800

    split

--- test1.php
+++ test1.php
@@ -2,7 +2,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
-$conn = new mysqli("localhost","root","galaxy123456@","eosdb"); 
+$conn = new mysqli("localhost","root","123","eosdb"); 
 
 echo "连接";
 
