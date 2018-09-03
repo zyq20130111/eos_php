8c5ccab19e71675982f8a2ef0962e133fa675404
System Administrator <root@zhangfanfandeMacBook-Pro.local>
2018-09-03 13:51:20 +0800

    split

--- test1.php
+++ test1.php
@@ -8,7 +8,7 @@ echo "连接";
 
 $inj_str = "'|and|exec|insert|select|delete|update|count|*|%|chr|mid|master|truncate|char|declare|;|or|-|+|,|drop";
 echo($inj_str);
-$inj_stra = split("|",$inj_str);
+$inj_stra = explode("|",$inj_str);
 $c = count($inj_str);
 /*
 for($i=0 ; $i < count($inj_str) ; $i++)
