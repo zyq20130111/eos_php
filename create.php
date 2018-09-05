9cf39f7327f6937f2b5ee7a8b7c4c99772393ee0
System Administrator <root@zhangfanfandeMacBook-Pro.local>
2018-09-05 15:12:05 +0800

    create.php

--- create.php
+++ create.php
@@ -27,7 +27,7 @@ function request_post($url = '', $param = '') {
      return $data;
 }
 
-//得到账号信息 -1表示账号存在，0表示账号存在
+//得到账号信息 -1表示账号不存在，0表示账号存在
 function getAccount($account){
 
    $flag = -1;
@@ -35,7 +35,7 @@ function getAccount($account){
    $data = request_post("http://127.0.0.1:8888/v1/chain/get_account",$post_data);
 
    $json = json_decode($data,true);
-   if(trim($json["account_name"]) == trim($account)){
+   if(isset($json["account_name"]) &&  (trim($json["account_name"]) == trim($account)) ){
         $flag = 0;
    }
 
