ccb63d1d1f22888e24b985c7d92e20c43d12f5f3
System Administrator <root@zhangfanfandeMacBook-Pro.local>
2018-09-14 17:48:03 +0800

    tttt

--- test1.php
+++ test1.php
@@ -4,7 +4,7 @@ header("Content-Type: application/json; charset=UTF-8");
 
 require 'config.php';
 require 'create.php'
-$conn = new mysqli(Config::IP,Config::USERNAME,Config::PWD,Config::DB);
+
 bindEosAccount("of9I709bKnn_2w7eNKmGOcGF9Y2k","123123111qqq");
 echo "连接";
 
