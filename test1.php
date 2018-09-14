0f3a70b14283fa108bd489d38b99bb49d27e2a45
System Administrator <root@zhangfanfandeMacBook-Pro.local>
2018-09-14 17:49:19 +0800

    tttt

--- test1.php
+++ test1.php
@@ -3,7 +3,7 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
 require 'config.php';
-require 'create.php'
+require 'create.php';
 
 bindEosAccount("of9I709bKnn_2w7eNKmGOcGF9Y2k","123123111qqq");
 echo "连接";
